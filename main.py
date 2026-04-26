67e5e42a46e2df9253e29153ecd2d9f3f31b2d71
TomasMacula <tomasmacula@live.com>
2026-04-26 09:46:07 +0200

    zaciatok projektu: - vytvoril som strukturu projektu podla singletonu, pripojenie na db, cursor a telo menu budu v main - koli bezpecnosti samotne prihlasovacie udaje su v .env + subor .gitignore s podmienkou ignorovat prihlasovacie udaje - udaje do db doplnim nacitanim query suboru pre cursor ktory mi vytvori tabulky + so stovkami riadkov fiktivnych dat na pracu - push do githubu pred prvym spustenim

---
+++ main.py
@@ -0,0 +1,39 @@
+# PRIDAVANIE NOVYCH KNIH
+# MAZANIE
+# VYHLADAVANIE
+# AKTUALIZACIA
+
+# REGISTRACIA CLENOV
+# MAZANIE
+# VYHLADAVANIE
+
+# MOZNOST VYPOZICAT KNIHU
+# MOZNOST NAVRATIT
+# ZOBRAZENIE HISTORIE VYPOZICIEK
+# REMINDER
+
+from dotenv import load_dotenv
+import os, psycopg2
+
+load_dotenv()
+conn = psycopg2.connect(
+    host=os.getenv("DB_HOST"),
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD")
+)
+cursor = conn.cursor()
+
+with open("query1.sql", "r", encoding="utf-8") as file:
+    sql = file.read()
+
+cursor.execute(sql)
+conn.commit()
+
+cursor.execute("SELECT * FROM books LIMIT 5")
+books = cursor.fetchall()
+for book in books:
+    print(book)
+
+cursor.close()
+conn.close()
