f8190d078955237a1fd3e52b8b1abab93a472c93
TomasMacula <tomasmacula@live.com>
2026-04-27 12:39:35 +0200

    - pridane funkcie do triedy Member - cez main otestovane funkcie

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os, psycopg2
 from book import Book
+from members import Member
 
 load_dotenv()
 conn = psycopg2.connect(
@@ -11,15 +12,22 @@ conn = psycopg2.connect(
 )
 cursor = conn.cursor()
 book = Book(cursor, conn)
+member = Member(cursor, conn)
 
-book.search("slabikar")
-book.add("Slabikar", 1, 2, 9780000000099, 2026, 3)
-book.search("slabikar")
-book.update(106, title="Zahradkar", copies=5)
-book.search("slabikar")
-book.search("zahradkar")
-book.delete("106")
-book.search("zahradkar")
+# book.search("slabikar")
+# book.add("Slabikar", 1, 2, 9780000000099, 2026, 3)
+# book.search("slabikar")
+# book.update(106, title="Zahradkar", copies=5)
+# book.search("slabikar")
+# book.search("zahradkar")
+# book.delete("106")
+# book.search("zahradkar")
+
+member.search("Peter")
+# member.add("Tomas", "Macula", "test@email.com")
+member.search("macula")
+member.delete("44")
+member.search("macula")
 
 # with open("Query_1.sql", "r", encoding="utf-8") as file:
 #     sql = file.read()
--- members.py
+++ members.py
@@ -3,3 +3,32 @@
 # VYHLADAVANIE
 
 class Member:
+    def __init__(self, cursor, conn):
+        self.cursor = cursor
+        self.conn = conn
+
+    def add(self, first_name, last_name, email):
+        self.cursor.execute("""
+            INSERT INTO members (first_name, last_name, email)
+            VALUES (%s, %s, %s)
+        """, (first_name, last_name, email))
+        self.conn.commit()
+        print("novy clen zaregistrovany")
+
+    def delete(self, member_id):
+        self.cursor.execute("DELETE FROM members WHERE member_id = %s", (member_id,))
+        self.conn.commit()
+
+    def search(self, key):
+        self.cursor.execute("""
+            SELECT * FROM members
+            WHERE first_name ILIKE %s
+            OR last_name ILIKE %s
+            OR email ILIKE %s
+        """, (f"%{key}%", f"%{key}%", f"%{key}%"))
+        results = self.cursor.fetchall()
+        if results:
+            for riadok in results:
+                print(riadok)
+        else:
+            print("clen nenajdeny")
