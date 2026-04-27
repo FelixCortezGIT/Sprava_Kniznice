1162c5118d92f6378f2516c14b2b6e83d2927c31
TomasMacula <tomasmacula@live.com>
2026-04-27 12:17:30 +0200

    - vytvorene funkcie add, delete, search a update pre class Book - v update opravene vsetky numericke stlpce kde hodnota nemoze byt 0 ako hodnota ktora nedava logicky zmysel, pre pocet kopii, rok publikacie, genre_id a author_id pretoze if copies python vyhodnoti 0 ako False cize None, na opravu pre tieto stlpce if author_id is not None: - pouzite %s kde sa hodnota doplni az pri execute na ochranu koli sql injection - cez main otestovane funkcie, pridavaju, upravuju, zobrazuju a mazu knihy v DB

--- book.py
+++ book.py
@@ -1,6 +1,62 @@
-# PRIDAVANIE NOVYCH KNIH
-# MAZANIE
-# VYHLADAVANIE
-# AKTUALIZACIA
-
 class Book:
+    def __init__(self, cursor, conn):
+        self.cursor = cursor
+        self.conn = conn
+
+    def add(self, title, author_id, genre_id, isbn, publication_year, copies):
+        self.cursor.execute("""
+            INSERT INTO books (title, author_id, genre_id, isbn, publication_year, copies)
+            VALUES (%s, %s, %s, %s, %s, %s)
+        """, (title, author_id, genre_id, isbn, publication_year, copies))
+        self.conn.commit()
+        print(f"kniha {title} bola pridana do zoznamu")
+
+    def delete(self, book_id):
+        self.cursor.execute("DELETE FROM books WHERE book_id = %s", (book_id,))
+        self.conn.commit()
+
+    def search(self, kluc):
+        self.cursor.execute("""
+            SELECT * FROM books b
+            JOIN authors a ON b.author_id = a.author_id
+            LEFT JOIN genres g ON b.genre_id = g.genre_id
+            WHERE b.title ILIKE %s
+            OR a.name ILIKE %s
+            OR g.name ILIKE %s
+        """, (f"%{kluc}%", f"%{kluc}%", f"%{kluc}%"))
+        results = self.cursor.fetchall()
+        if results:
+            for riadok in results:
+                print(riadok)
+        else:
+            print("kniha nebola najdena")
+
+    def update(self, book_id, title=None, author_id=None, genre_id=None, isbn=None, publication_year=None, copies=None):
+        fields = []
+        values = []
+        if title:
+            fields.append("title = %s")
+            values.append(title)
+        if author_id is not None:
+            fields.append("author_id = %s")
+            values.append(author_id)
+        if genre_id is not None:
+            fields.append("genre_id = %s")
+            values.append(genre_id)
+        if isbn:
+            fields.append("isbn = %s")
+            values.append(isbn)
+        if publication_year is not None:
+            fields.append("publication_year = %s")
+            values.append(publication_year)
+        if copies is not None:
+            fields.append("copies = %s")
+            values.append(copies)
+        if not fields:
+            print("ziadne udaje na aktualizaciu")
+            return
+        values.append(book_id)
+        query = "UPDATE books SET " + ', '.join(fields) + " WHERE book_id = %s"
+        self.cursor.execute(query, tuple(values))
+        self.conn.commit()
+        print(f"kniha s id {book_id} bola aktualizovana")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os, psycopg2
+from book import Book
 
 load_dotenv()
 conn = psycopg2.connect(
@@ -9,16 +10,26 @@ conn = psycopg2.connect(
     password=os.getenv("DB_PASSWORD")
 )
 cursor = conn.cursor()
+book = Book(cursor, conn)
+
+book.search("slabikar")
+book.add("Slabikar", 1, 2, 9780000000099, 2026, 3)
+book.search("slabikar")
+book.update(106, title="Zahradkar", copies=5)
+book.search("slabikar")
+book.search("zahradkar")
+book.delete("106")
+book.search("zahradkar")
 
 # with open("Query_1.sql", "r", encoding="utf-8") as file:
 #     sql = file.read()
 # cursor.execute(sql)
 # conn.commit()
 
-cursor.execute("SELECT * FROM books LIMIT 5")
-books = cursor.fetchall()
-for book in books:
-    print(book)
+# cursor.execute("SELECT * FROM books LIMIT 5")
+# books = cursor.fetchall()
+# for book in books:
+#     print(book)
 
 cursor.close()
 conn.close()
